18c3594285ba549dbea9850b5ba8fb19c376bf0d
Tobias Sgoff <tobias.sgoff@seven.one>
2022-05-10 16:00:23 +0200

    udpate terraform s3

--- terraform/env/int/main.tf
+++ terraform/env/int/main.tf
@@ -8,7 +8,7 @@ locals {
 
 module "mongodb" {
   source = "../../apps/mongodb"
-  tags   = local.env_tags
+  tags                    = local.env_tags
   disable_api_termination = false
   instance_count          = 3
   instance_type           = "t4g.nano"
